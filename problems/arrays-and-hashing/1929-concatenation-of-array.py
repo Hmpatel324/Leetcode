9306e9eec7407040f350e1d5253955a11059eeda
Harshil Patel <hmpatel324@hotmail.com>
2025-04-03 18:35:18 -0400

    1929-array

---
+++ problems/arrays-and-hashing/1929-concatenation-of-array.py
@@ -0,0 +1,25 @@
+"""
+Link: https://leetcode.com/problems/concatenation-of-array
+Difficulty: easy
+
+Problem:
+concat two lists
+
+Input / Output:
+
+
+Notes:
+
+
+Solution Approaches:
+list addition
+
+"""
+
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return nums + nums
